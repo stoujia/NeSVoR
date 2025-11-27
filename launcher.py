f51e12fdbe4672155ce99641acc96219cff13aeb
stoujia <jia.stou@gmail.com>
2025-11-27 10:34:09 +0100

    with bg and training time

--- launcher.py
+++ launcher.py
@@ -4,15 +4,17 @@ import argparse
 import sys
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, required=True, help="Path to subjects.yaml")
-    # You can pass global tuning params here to forward them to the worker
-    parser.add_argument("--resolution", type=str, default="0.8")
-    parser.add_argument("--iterations", type=str, default="3000")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--config", type=str, required=True, help="Path to subjects.yaml")
+    # # You can pass global tuning params here to forward them to the worker
+    # # parser.add_argument("--resolution", type=str, default="0.5")
+    # # parser.add_argument("--iterations", type=str, default="3000")
+    # args = parser.parse_args()
 
     # 1. Read the YAML to get the list of jobs
-    with open(args.config, 'r') as f:
+    config_path = "subjects.yaml"
+
+    with open(config_path, 'r') as f:
         data = yaml.safe_load(f)
 
     subjects = data.get('subjects', {})
@@ -31,11 +33,11 @@ def main():
             # This launches a completely new Python instance
             cmd = [
                 sys.executable, "run.py",
-                "--config", args.config,
+                "--config", config_path,
                 "--subject", sub_id,
                 "--session", ses_id,
-                "--resolution", args.resolution,
-                "--iterations", args.iterations
+                # "--resolution", args.resolution,
+                # "--iterations", args.iterations
             ]
 
             # 4. Run and wait
